c8d48451de70c00f96f841c92f2443fae694beb4
Nsingletary <singletarybush@gmail.com>
2020-11-28 21:11:19 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,12 @@
+class Dog 
+    def name = (dog_name)
+        this_dogs_name= dog dog_name
+    end
+    def name 
+        this _dogs_name 
+    end
+end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+lassie.anem #=> "Lassie"
